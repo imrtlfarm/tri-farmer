52482df030f22b22a9adb6f022f23a2221f640e4
Magnus Brantheim <m.brantheim@gmail.com>
2022-04-08 12:25:47 +0200

    Deployed FTM-AVAX

--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,6 +1,6 @@
 async function main() {
-  const vaultAddress = '';
-  const strategyAddress = '';
+  const vaultAddress = '0xb71cE39DDf968da084bF1eF2D7B9525C284852f4';
+  const strategyAddress = '0xf28027f51a265a0bd496372f5859Ed95CF51CFcB';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
   const vault = Vault.attach(vaultAddress);

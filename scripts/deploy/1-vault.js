ee44d71aee35d0bf546a4b2ac3a7603478540a84
Magnus Brantheim <m.brantheim@gmail.com>
2022-04-08 13:39:40 +0200

    Deployed FTM-MST

--- scripts/deploy/1-vault.js
+++ scripts/deploy/1-vault.js
@@ -1,9 +1,9 @@
 async function main() {
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
 
-  const wantAddress = '0xC9FB686f14bDA7e2653cF8F605Dc8551B6a53FD3';
-  const tokenName = 'FTM-APE Spooky Crypt';
-  const tokenSymbol = 'rf-FTM-APE';
+  const wantAddress = '0x1a88E447c7468B28de490b25a076A4fFc0C68b16';
+  const tokenName = 'FTM-MST Spooky Crypt';
+  const tokenSymbol = 'rf-FTM-MST';
   const depositFee = 0;
   const tvlCap = ethers.constants.MaxUint256;
 
--- scripts/deploy/3-initialize.js
+++ scripts/deploy/3-initialize.js
@@ -1,6 +1,6 @@
 async function main() {
-  const vaultAddress = '0xb1f916892c649EB163328F952642199F106C08b4';
-  const strategyAddress = '0x99DA24ce0Ae29A0D8448f0a9821334c68c23aAC8';
+  const vaultAddress = '0xb03ED40Ba9df099D71eB2d4CB3AE52aC06147170';
+  const strategyAddress = '0xBC4508E6dFa08cb301066C35a723F331A484a2DE';
 
   const Vault = await ethers.getContractFactory('ReaperVaultv1_4');
   const vault = Vault.attach(vaultAddress);
